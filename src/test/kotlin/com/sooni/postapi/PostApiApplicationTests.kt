7a72744e12efafcf98b1a2b2e6bede2a9598b4fb
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-16 21:33:28 +0900

    first commit

---
+++ src/test/kotlin/com/sooni/postapi/PostApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sooni.postapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
