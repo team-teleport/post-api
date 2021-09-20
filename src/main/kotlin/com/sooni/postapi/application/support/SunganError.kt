970f17d607368971ed962670d39a1c43395272dd
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-20 23:21:00 +0900

    feat: error, exception, response Object

---
+++ src/main/kotlin/com/sooni/postapi/application/support/SunganError.kt
@@ -0,0 +1,21 @@
+package com.sooni.postapi.application.support
+
+import org.springframework.http.HttpStatus
+
+enum class SunganError(
+    val desc: String, val status: HttpStatus
+) {
+    // 4xx
+    DUPLICATE("중복 입력입니다.", HttpStatus.BAD_REQUEST),
+    BAD_REQUEST("사용자의 입력이 잘못되었습니다.", HttpStatus.BAD_REQUEST),
+    NOT_LOGIN("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
+    FORBIDDEN("권한이 없는 유저 입니다.", HttpStatus.FORBIDDEN),
+    DELETD_USER("삭제된 유저입니다.", HttpStatus.FORBIDDEN),
+
+    // 5xx
+    FAIL_TO_CREATE("db 에러 - 생성 실패", HttpStatus.INTERNAL_SERVER_ERROR),
+    ENTITY_NOT_FOUND("db 에러 - 엔터티를 찾을 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    AWS_ERROR("AWS 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNEXPECTED_VALUE("예상하지 못한 값입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNKNOWN_ERROR("알 수 없는 에러", HttpStatus.INTERNAL_SERVER_ERROR),
+}
---
+++ src/main/kotlin/com/sooni/postapi/application/support/SunganException.kt
@@ -0,0 +1,13 @@
+package com.sooni.postapi.application.support
+
+class SunganException : RuntimeException {
+    var error: SunganError = SunganError.UNKNOWN_ERROR
+
+    constructor(error: SunganError) : super(error.desc) {
+        this.error = error
+    }
+
+    constructor(error: SunganError, message: String) : super(error.desc + ": " + message) {
+        this.error = error
+    }
+}
