eede9d68121dac66c80cc4f608fa111f35532e13
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-03 01:21:25 +0900

    feat: sunganQueryRepository

--- src/main/kotlin/com/sungan/postApi/repository/SunganRepository.kt
+++ src/main/kotlin/com/sungan/postApi/repository/SunganRepository.kt
@@ -3,5 +3,5 @@ package com.sungan.postApi.repository
 import com.sungan.postApi.domain.Sungan
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SunganRepository: JpaRepository<Sungan, Long>{
+interface SunganRepository: JpaRepository<Sungan, Long>, SunganQueryRepository{
 }
---
+++ src/main/kotlin/com/sungan/postApi/repository/UserViewdSunganRepository.kt
@@ -0,0 +1,7 @@
+package com.sungan.postApi.repository
+
+import com.sungan.postApi.domain.UserViewdSungan
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserViewdSunganRepository:JpaRepository<UserViewdSungan, Long> {
+}
