113fad051c8bc6297f05b67eedbc090beafb602c
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-04 17:44:34 +0900

    fix: request dto에 orderby 필드 추가
    * OrderType은 숫자로 작성하면 된다. 0, 1, 2

--- src/main/kotlin/com/sungan/postApi/dto/Main.kt
+++ src/main/kotlin/com/sungan/postApi/dto/Main.kt
@@ -4,10 +4,19 @@ import io.swagger.annotations.ApiModelProperty
 
 class Main
 
-data class GetMainRequestDto (
+data class GetMainRequestDto(
     @ApiModelProperty(example = "100")
     val lastSunganId: Long = 10000000, // TODO: 여기 어떻게 할지 정하기
 
     @ApiModelProperty(required = true, example = "2호선")
-    val vehicleName: String
-        )
+    val vehicleName: String,
+
+    @ApiModelProperty(value = "정렬 기준(0: 최신순, 1: 좋아요순, 2: 조회수순)", required = true, example = "0")
+    val orderBy: OrderType
+)
+
+enum class OrderType {
+    NEW,
+    LIKE,
+    READ
+}
