1e13cdabb407e6adf4d591d088016574f310d253
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-12 02:34:58 +0900

    feat: api 두개 구현

--- src/main/kotlin/com/sungan/postApi/dto/Main.kt
+++ src/main/kotlin/com/sungan/postApi/dto/Main.kt
@@ -9,7 +9,11 @@ data class GetMainRequestDto(
     val vehicleName: String,
 
     @ApiModelProperty(value = "정렬 기준(0: 최신순, 1: 좋아요순, 2: 조회수순)", required = true, example = "0")
-    val orderBy: OrderType
+    val orderBy: OrderType,
+
+    @ApiModelProperty(value = "가져오고 싶은 순간 개수")
+    val size: Long,
+
 )
 
 enum class OrderType {
@@ -17,3 +21,8 @@ enum class OrderType {
     LIKE,
     READ
 }
+
+data class SunganWithLikeByUser(
+    val sungan: SunganVo,
+    val isLiked: Boolean
+)
