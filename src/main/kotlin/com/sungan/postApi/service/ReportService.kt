2ec08b96bfbb9fe2c0cd5e5a6223bec83c70c116
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-28 01:38:21 +0900

    fix: service에 dto직접 넣어주기
    userInfo 생성을 위하여

--- src/main/kotlin/com/sungan/postApi/service/ReportService.kt
+++ src/main/kotlin/com/sungan/postApi/service/ReportService.kt
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service
 @Service
 class ReportService(
     val reportRepository: ReportRepository,
-    val stationRepository: Line2StationRepository,
     val reportCommentRepository: ReportCommentRepository,
     val reportCommentLikeRepository: ReportCommentLikeRepository,
     val reportNestedCommentRepository: ReportNestedCommentRepository,
