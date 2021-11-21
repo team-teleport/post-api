ea9a79b9748f1442ec339fb229beb1d7aebe6b9a
kayoung yoon <rkdkdudjd@gmail.com>
2021-11-22 02:28:09 +0900

    feat: 순간댓글 좋아요 구현

--- src/main/kotlin/com/sungan/postApi/service/CommentLikeService.kt
+++ src/main/kotlin/com/sungan/postApi/service/CommentLikeService.kt
@@ -4,6 +4,8 @@ import com.sungan.postApi.application.support.SunganError
 import com.sungan.postApi.application.support.SunganException
 import com.sungan.postApi.domain.sungan.CommentLike
 import com.sungan.postApi.dto.CommentLikeVo
+import com.sungan.postApi.event.publisher.LikeType
+import com.sungan.postApi.event.publisher.NotiEventPublisher
 import com.sungan.postApi.repository.CommentLikeRepository
 import com.sungan.postApi.repository.CommentRepository
 import org.springframework.stereotype.Service
@@ -11,17 +13,20 @@ import org.springframework.stereotype.Service
 @Service
 class CommentLikeService(
     val commentLikeRepository: CommentLikeRepository,
-    val commentRepository: CommentRepository
+    val commentRepository: CommentRepository,
+    val notiEventPublisher: NotiEventPublisher,
 ) {
     fun createCommentLike(userId: Long, commentId: Long): CommentLikeVo {
         val comment =
             commentRepository.findById(commentId).orElseThrow { throw SunganException(SunganError.BAD_REQUEST) }
         commentLikeRepository.findByUserIdAndComment(userId, comment)
             ?.let { throw SunganException(SunganError.DUPLICATE) }
-        return commentLikeRepository.save(CommentLike(
+        val newLike = commentLikeRepository.save(CommentLike(
             userId,
             commentRepository.findById(commentId).orElseThrow { throw SunganException(SunganError.BAD_REQUEST) }
-        )).convertToVo()
+        ))
+        notiEventPublisher.publishLikeRegisteredEvent(comment.userInfo.userId, newLike.userId, LikeType.Comment)
+        return newLike.convertToVo()
     }
 
     fun destroyCommentLike(userId: Long, commentId: Long) {
